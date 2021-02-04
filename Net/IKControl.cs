577f0d712dd8e8a22eebb3c94d9c7d2d6b07fa5c
gggraph <62741099+gggraph@users.noreply.github.com>
2021-02-04 13:50:33 +0100

    Add files via upload

---
+++ Net/IKControl.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKControl : MonoBehaviour
+{
+
+    protected Animator animator;
+
+    public bool ikActive = true; // set to true
+    public Transform rightHandObj = null; // have to be found somewhere aha !!! 
+    public Transform leftHandObj = null; // have to be found somewhere aha !!! 
+    public Transform lookObj = null;
+    public Transform headTransform = null;
+    private GameObject HeadObj;
+    private GameObject Center;
+
+   // private GameObject GaelFace;
+   // private GameObject LionelFace;
+
+
+    void Start()
+    {
+        rightHandObj = this.transform.parent.transform.Find("RightHand");
+        leftHandObj = this.transform.parent.transform.Find("LeftHand");
+        lookObj = this.transform.parent.transform.Find("RightHand");
+        animator = GetComponent<Animator>();
+        HeadObj = this.transform.parent.transform.Find("Head").gameObject;
+        headTransform = HeadObj.transform;
+        Center = this.transform.parent.transform.Find("Center").gameObject;
+        //GaelFace = this.transform.parent.transform.Find("GAEL").gameObject;
+        //LionelFace = this.transform.parent.transform.Find("Lionel").gameObject;
+
+    }
+
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            //if the IK is active, set the position and rotation directly to the goal. 
+            if (ikActive)
+            {
+                // Set the look target position, if one has been assigned
+                if (lookObj != null)
+                {
+                    animator.SetLookAtWeight(1);
+                    Vector3 lookPosition = HeadObj.transform.position + HeadObj.transform.forward * 2.0f;
+                    animator.SetLookAtPosition(lookPosition);
+                }
+
+                // Set the right hand target position and rotation, if one has been assigned
+                if (rightHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                   
+                }
+                if (leftHandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
+                 
+                }
+
+            }
+
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        //new Vector3(Center.transform.position.x, Center.transform.position.y , Center.transform.position.z);// prendre 'objet XR RIG
+        // update body transform : 
+        // keep head for positioning. center for right high 
+        this.transform.position = new Vector3(HeadObj.transform.position.x, Center.transform.position.y - 0.55f, HeadObj.transform.position.z); // 0.7 trop haut
+        this.transform.position -= this.transform.forward * 0.2f; // valeur a changer 0.2 niquel ?
+        this.transform.rotation = Center.transform.rotation; 
+    }
+}
---
+++ Net/NetMaster.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetMaster : MonoBehaviourPunCallbacks
+{
+    private string _RoomName = "CyberCave";
+    public GameObject PlayerPrefab;
+
+    /*
+     Ficher de Sauvegarde automatique
+     * ma derniere position
+     */
+    private void Start()
+    {
+        // for testing purpose
+        _RoomName = "CyberCave";
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public void Init(string roomInfo = "CyberCave")
+    {
+
+        _RoomName = roomInfo;
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public void InstantiateAvatar()
+    {
+        // Cherche un prefab appelé player dans /Resources
+        PhotonNetwork.Instantiate("Avatar 2", new Vector3(0, 0, 0), Quaternion.identity, 0);
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master.");
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 6;
+
+        PhotonNetwork.JoinOrCreateRoom(_RoomName, options, TypedLobby.Default);
+
+
+        DestroyRoomPrefab();
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room created!");
+        base.OnCreatedRoom();
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed : " + message);
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Room successfully joined!");
+        InstantiateAvatar();
+        base.OnJoinedRoom();
+    }
+
+
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Joining room failed :  " + message);
+        base.OnJoinRoomFailed(returnCode, message);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo inf in roomList)
+        {
+            Debug.Log(inf.Name);
+            Debug.Log(inf.MaxPlayers);
+            Debug.Log(inf.IsOpen);
+            Debug.Log(inf.IsVisible);
+        }
+        base.OnRoomListUpdate(roomList);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby successfully! ");
+        base.OnJoinedLobby();
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected from server because " + cause.ToString());
+        base.OnDisconnected(cause);
+    }
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Joining " + _RoomName + " failed ! ");
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+
+    void DestroyRoomPrefab()
+    {
+       // Destroy(GameObject.Find("RoomInfo").gameObject);
+
+    }
+
+    [PunRPC]
+    void SwitchFaceCommand(PhotonMessageInfo pif)
+    {
+        GameObject[] allplayer = GameObject.FindGameObjectsWithTag("Avatar"); 
+        foreach ( GameObject p in allplayer) 
+        { 
+            if ( p.GetComponent<PhotonView>() == pif.photonView) 
+            {
+                GameObject GaelFace = p.transform.Find("GAEL").gameObject;
+                GameObject LionelFace = p.transform.Find("LIONEL").gameObject;
+
+                if (LionelFace.activeInHierarchy)
+                {
+                    LionelFace.SetActive(false);
+                    GaelFace.SetActive(true);
+                    return;
+
+                }
+                else
+                {
+                    LionelFace.SetActive(true);
+                    GaelFace.SetActive(false);
+                    return;
+                }
+
+                
+            }
+        
+        }
+
+    }
+
+}
+
